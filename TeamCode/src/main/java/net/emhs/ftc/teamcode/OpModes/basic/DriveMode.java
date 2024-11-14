42eeeea291d608dd93de6fc58d35e15ea4fb4881
evcann <97171419+evanh08@users.noreply.github.com>
2024-11-14 10:37:33 -0500

    Added arm motor and placeholder code for face button presets

--- TeamCode/src/main/java/net/emhs/ftc/teamcode/OpModes/basic/DriveMode.java
+++ TeamCode/src/main/java/net/emhs/ftc/teamcode/OpModes/basic/DriveMode.java
@@ -7,25 +7,25 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 @TeleOp(name = "Drive Mode", group = "default")
 public class DriveMode extends LinearOpMode {
 
-    private DcMotor frontLeft, frontRight, backLeft, backRight;
+    private DcMotor frontLeft, frontRight, backLeft, backRight, arm;
+
     double rightX1, rightY1, leftX1, leftY1, rightX2, rightY2, leftX2, leftY2, rightTrigger1, leftTrigger1, rightTrigger2, leftTrigger2;
     public double speed = 1;
 
     // Change these values as needed
     // All are from zero to one
-    double defaultSpeed = 0.75; // Speed when no analog triggers are pressed.
-    double minSpeed = 0.20; // The minimum speed when slowing down using analog triggers. (Left trigger)
-    double maxSpeed = 1.00; // The maximum speed when speeding up using analog triggers. (Right trigger)
+    final double defaultSpeed = 0.75; // Speed when no analog triggers are pressed.
+    final double minSpeed = 0.20; // The minimum speed when slowing down using analog triggers. (Left trigger)
+    final double maxSpeed = 1.00; // The maximum speed when speeding up using analog triggers. (Right trigger)
+
+    final double armRaiseSpeed = 0.8; // The speed at which the arm is raised
+    final double armLowerSpeed = 0.5;
 
     @Override
     public void runOpMode() throws InterruptedException {
-        frontRight = hardwareMap.get(DcMotor.class, "frontRight");
-        frontLeft = hardwareMap.get(DcMotor.class, "frontLeft");
-        backRight = hardwareMap.get(DcMotor.class, "backRight");
-        backLeft = hardwareMap.get(DcMotor.class, "backLeft");
+        setUpMotors();
 
         waitForStart();
-
 
         while(opModeIsActive()) {
             updateVariables();
@@ -40,6 +40,23 @@ public class DriveMode extends LinearOpMode {
         backLeft.setPower(-((leftY1 + leftX1 + rightX1) / denominator)*speed);
         frontRight.setPower(((leftY1 + leftX1 - rightX1) / denominator)*speed);
         backRight.setPower(((leftY1 - leftX1 - rightX1) / denominator)*speed);
+
+        // All arm motion should be in this if statement to prevent conflicts
+        if (rightY2 != 0) { // Manual control takes priority (Controller 2, Right stick)
+            arm.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            arm.setPower(rightY2); // Manual arm control with right stick Y
+        } else if (gamepad2.a) {
+            // A button preset
+            // runArmToPos(26935);
+        } else if (gamepad2.b) {
+            // B button preset
+        } else if (gamepad2.x) {
+            // X button preset
+        } else if (gamepad2.y) {
+            // Y button preset
+        }
+
+
     }
 
     private void updateVariables() {
@@ -61,5 +78,36 @@ public class DriveMode extends LinearOpMode {
         subtracts the left trigger.
         */
 
+    }
+
+    private void setUpMotors() {
+
+        /*
+        DC MOTORS:
+        First four are the mecanum wheels
+        Arm controls the height of the collapsible arm
+         */
+
+        frontRight = hardwareMap.get(DcMotor.class, "frontRight");
+        frontLeft = hardwareMap.get(DcMotor.class, "frontLeft");
+        backRight = hardwareMap.get(DcMotor.class, "backRight");
+        backLeft = hardwareMap.get(DcMotor.class, "backLeft");
+        arm = hardwareMap.get(DcMotor.class, "arm");
+
+        DcMotor[] motors = { // Putting all DC Motors in an array allows for modifying each with a for loop
+             frontRight, frontLeft, backRight, backLeft, arm
+        };
+
+        for (DcMotor motor: motors) { // For each DC Motor in the array
+            motor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            motor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        }
+
+
+    }
+
+    private void runArmToPos (int pos) {
+        arm.setTargetPosition(pos);
+        arm.setMode(DcMotor.RunMode.RUN_TO_POSITION);
     }
 }
