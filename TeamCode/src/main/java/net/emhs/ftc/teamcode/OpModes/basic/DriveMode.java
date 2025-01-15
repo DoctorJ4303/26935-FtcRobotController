af69e1b892dbd482881d28ed1e90358d482d0bf1
DoctorJ4303 <78428443+DoctorJ4303@users.noreply.github.com>
2025-01-15 17:01:43 -0500

    Added touch sensor and update a few of the motors/servos

--- TeamCode/src/main/java/net/emhs/ftc/teamcode/OpModes/basic/DriveMode.java
+++ TeamCode/src/main/java/net/emhs/ftc/teamcode/OpModes/basic/DriveMode.java
@@ -4,6 +4,7 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.TouchSensor;
 
 import org.firstinspires.ftc.robotcore.external.Func;
 import org.firstinspires.ftc.robotcore.external.Telemetry;
@@ -13,6 +14,7 @@ public class DriveMode extends LinearOpMode {
 
     private DcMotor frontLeft, frontRight, backLeft, backRight, armLift, slider;
     private Servo claw, wrist, elbow1, elbow2, tilt, brush;
+    private TouchSensor endStop;
 
     double rightX1, rightY1, leftX1, leftY1, rightX2, rightY2, leftX2, leftY2, rightTrigger1, leftTrigger1, rightTrigger2, leftTrigger2;
     public double speed = 1;
@@ -32,141 +34,15 @@ public class DriveMode extends LinearOpMode {
     public void runOpMode() throws InterruptedException {
         setUpDcMotors();
         setUpServos();
+        endStop = hardwareMap.get(TouchSensor.class, "endStop");
 
         waitForStart();
-
-        Telemetry telemetry = new Telemetry() {
-            @Override
-            public Item addData(String caption, String format, Object... args) {
-                return null;
-            }
-
-            @Override
-            public Item addData(String caption, Object value) {
-                return null;
-            }
-
-            @Override
-            public <T> Item addData(String caption, Func<T> valueProducer) {
-                return null;
-            }
-
-            @Override
-            public <T> Item addData(String caption, String format, Func<T> valueProducer) {
-                return null;
-            }
-
-            @Override
-            public boolean removeItem(Item item) {
-                return false;
-            }
-
-            @Override
-            public void clear() {
-
-            }
-
-            @Override
-            public void clearAll() {
-
-            }
-
-            @Override
-            public Object addAction(Runnable action) {
-                return null;
-            }
-
-            @Override
-            public boolean removeAction(Object token) {
-                return false;
-            }
-
-            @Override
-            public void speak(String text) {
-
-            }
-
-            @Override
-            public void speak(String text, String languageCode, String countryCode) {
-
-            }
-
-            @Override
-            public boolean update() {
-                return false;
-            }
-
-            @Override
-            public Line addLine() {
-                return null;
-            }
-
-            @Override
-            public Line addLine(String lineCaption) {
-                return null;
-            }
-
-            @Override
-            public boolean removeLine(Line line) {
-                return false;
-            }
-
-            @Override
-            public boolean isAutoClear() {
-                return false;
-            }
-
-            @Override
-            public void setAutoClear(boolean autoClear) {
-
-            }
-
-            @Override
-            public int getMsTransmissionInterval() {
-                return 0;
-            }
-
-            @Override
-            public void setMsTransmissionInterval(int msTransmissionInterval) {
-
-            }
-
-            @Override
-            public String getItemSeparator() {
-                return "";
-            }
-
-            @Override
-            public void setItemSeparator(String itemSeparator) {
-
-            }
-
-            @Override
-            public String getCaptionValueSeparator() {
-                return "";
-            }
-
-            @Override
-            public void setCaptionValueSeparator(String captionValueSeparator) {
-
-            }
-
-            @Override
-            public void setDisplayFormat(DisplayFormat displayFormat) {
-
-            }
-
-            @Override
-            public Log log() {
-                return null;
-            }
-        };
 
         while(opModeIsActive()) {
             updateVariables();
             updateMovement();
 
-            telemetry.addData("Arm Position: ", armLift.getCurrentPosition());
+            telemetry.addData("", endStop.isPressed() ? "UwU" : "Touch me daddy");
             telemetry.update();
         }
     }
@@ -219,9 +95,9 @@ public class DriveMode extends LinearOpMode {
         }
         // Slider movement
         if (gamepad2.right_stick_y > 0){
-            slider.setPower(gamepad2.right_stick_y);
+            slider.setPower(gamepad2.right_stick_y*.1);
         }else if (gamepad2.right_stick_y < 0){
-            slider.setPower(gamepad2.right_stick_y);
+            slider.setPower(gamepad2.right_stick_y*.1);
         }
         //tilt movement
         if (gamepad2.right_stick_x > 0){
@@ -271,7 +147,8 @@ public class DriveMode extends LinearOpMode {
         frontLeft = hardwareMap.get(DcMotor.class, "frontLeft");
         backRight = hardwareMap.get(DcMotor.class, "backRight");
         backLeft = hardwareMap.get(DcMotor.class, "backLeft");
-        armLift = hardwareMap.get(DcMotor.class, "armLift");
+        armLift = hardwareMap.get(DcMotor.class, "armLift1");
+        armLift = hardwareMap.get(DcMotor.class, "armLift2");
         slider = hardwareMap.get(DcMotor.class, "slider");
 
         DcMotor[] motors = { // Putting all DC Motors in an array allows for modifying each with a for loop
